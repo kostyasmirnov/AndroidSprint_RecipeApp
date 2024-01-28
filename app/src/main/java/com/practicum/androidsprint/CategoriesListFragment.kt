261265cf1b8ee746fc8505aff40d5e42ee0bbb74
kasmirnov <kasmirnov@sigma-it.ru>
2024-01-28 13:31:54 +0300

    RA-10 fix. accessing the list of categories via STUB

--- app/src/main/java/com/practicum/androidsprint/CategoriesListFragment.kt
+++ app/src/main/java/com/practicum/androidsprint/CategoriesListFragment.kt
@@ -16,44 +16,8 @@ class CategoriesListFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    private val categories = listOf<Category>(
-        Category(
-            0,
-            "Бургеры",
-            "Рецепты всех популярных видов бургеров",
-            "burger.png",
-        ),
-        Category(
-            1,
-            "Десерты",
-            "Самые вкусные рецепты десертов специально для вас",
-            "dessert.png",
-        ),
-        Category(
-            2,
-            "Пицца",
-            "Пицца на любой вкус и цвет. Лучшая подборка для тебя",
-            "pizza.png",
-        ),
-        Category(
-            3,
-            "Рыба",
-            "Печеная, жареная, сушеная, любая рыба на твой вкус",
-            "fish.png",
-        ),
-        Category(
-            4,
-            "Супы",
-            "От классики до экзотики: мир в одной тарелке",
-            "soup.png",
-        ),
-        Category(
-            5,
-            "Салаты",
-            "Хрустящий калейдоскоп под соусом вдохновения",
-            "salad.png",
-        ),
-    )
+    private val categories: List<Category>
+        get() = STUB.getCategories()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
