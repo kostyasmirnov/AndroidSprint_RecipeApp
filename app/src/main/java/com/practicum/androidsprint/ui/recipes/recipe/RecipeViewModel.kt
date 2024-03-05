c6fe7ffd566ed870fc4837923b1b21ddeeb8221b
kasmirnov <kasmirnov@sigma-it.ru>
2024-03-05 18:30:13 +0300

    RA-20-fix

--- app/src/main/java/com/practicum/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/practicum/androidsprint/ui/recipes/recipe/RecipeViewModel.kt
@@ -5,13 +5,10 @@ import androidx.lifecycle.ViewModel
 import com.practicum.androidsprint.model.Recipe
 
 data class RecipeUiState(
-    val favoriteRecipe: Boolean,
-    val portionsCount: Int,
-)
-
-class RecipeViewModel (
     val recipe: Recipe? = null,
     val recipeDrawable: Drawable? = null,
     val portionsCount: Int? = 1,
     var isFavorite: Boolean = false,
-) : ViewModel()
+)
+
+class RecipeViewModel: ViewModel()
