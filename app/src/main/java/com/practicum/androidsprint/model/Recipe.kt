6cb930fc67d6a19d43c052f6104d06841eb0ec3f
kasmirnov <kasmirnov@sigma-it.ru>
2024-03-10 20:28:19 +0300

    RA-23 fix change to val

--- app/src/main/java/com/practicum/androidsprint/model/Recipe.kt
+++ app/src/main/java/com/practicum/androidsprint/model/Recipe.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Recipe(
-    var id: Int,
+    val id: Int,
     val title: String,
     val ingredients: List<Ingredient>,
     val method: List<String>,
